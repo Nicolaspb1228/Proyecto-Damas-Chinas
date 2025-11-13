70bdc25d4c25f1d20f829ae5aac35bd12b580da4
Nicolas Perez <nicolaspb1228@gmail.com>
2025-11-12 21:41:44 -0500

    Create roles.h

---
+++ roles.h
@@ -0,0 +1,18 @@
+#ifndef ROLES_H
+#define ROLES_H
+
+#include "estructuras.h"
+#include <string>
+
+// Funciones de gestión de roles
+void inicializarRol(Rol* rol, TipoRol tipo);
+void mostrarRolesDisponibles();
+TipoRol seleccionarRol(const std::string& nombreJugador);
+void mostrarHabilidadesRol(const Rol* rol);
+void mostrarDetalleRol(TipoRol tipo);
+void actualizarCooldowns(Rol* rol);
+bool puedeUsarHabilidad(const Rol* rol, int indiceHabilidad);
+void usarHabilidad(Rol* rol, int indiceHabilidad);
+std::string obtenerNombreRol(TipoRol tipo);
+
+#endif
