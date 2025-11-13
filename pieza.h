9b567baa70201c796517559e9917d80bda122b43
Nicolas Perez <nicolaspb1228@gmail.com>
2025-11-12 21:39:36 -0500

    Create pieza.h

---
+++ pieza.h
@@ -0,0 +1,15 @@
+#ifndef PIEZA_H
+#define PIEZA_H
+
+#include "estructuras.h"
+
+// Funciones para manejo de piezas
+void inicializarPieza(Pieza* pieza, int fila, int col, char color);
+char obtenerSimbolo(const Pieza* pieza);
+void coronarPieza(Pieza* pieza);
+bool esDamaPieza(const Pieza* pieza);
+char obtenerColorPieza(const Pieza* pieza);
+bool estaActivaPieza(const Pieza* pieza);
+void desactivarPieza(Pieza* pieza);
+
+#endif
