fc0a9981af97b19ff785106245da9a4350d16678
Nicolas Perez <nicolaspb1228@gmail.com>
2025-11-12 21:38:27 -0500

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+// Funciones para el menú
+int mostrarMenuPrincipal();
+void mostrarReglas();
+void mostrarCreditos();
+void limpiarPantalla();
+void pausar();
+
+#endif
